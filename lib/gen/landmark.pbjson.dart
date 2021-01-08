668e6bd9eec48b7d4d0a99f49631dff7d1b5d3f9
afonsopbarros <afonso.soveral.barros@gmail.com>
2021-01-08 12:14:15 +0000

    init

---
+++ lib/gen/landmark.pbjson.dart
@@ -0,0 +1,43 @@
+///
+//  Generated code. Do not modify.
+//  source: landmark.proto
+//
+// @dart = 2.7
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+const Landmark$json = const {
+  '1': 'Landmark',
+  '2': const [
+    const {'1': 'x', '3': 1, '4': 1, '5': 2, '10': 'x'},
+    const {'1': 'y', '3': 2, '4': 1, '5': 2, '10': 'y'},
+    const {'1': 'z', '3': 3, '4': 1, '5': 2, '10': 'z'},
+    const {'1': 'visibility', '3': 4, '4': 1, '5': 2, '10': 'visibility'},
+    const {'1': 'presence', '3': 5, '4': 1, '5': 2, '10': 'presence'},
+  ],
+};
+
+const LandmarkList$json = const {
+  '1': 'LandmarkList',
+  '2': const [
+    const {'1': 'landmark', '3': 1, '4': 3, '5': 11, '6': '.mediapipe.Landmark', '10': 'landmark'},
+  ],
+};
+
+const NormalizedLandmark$json = const {
+  '1': 'NormalizedLandmark',
+  '2': const [
+    const {'1': 'x', '3': 1, '4': 1, '5': 2, '10': 'x'},
+    const {'1': 'y', '3': 2, '4': 1, '5': 2, '10': 'y'},
+    const {'1': 'z', '3': 3, '4': 1, '5': 2, '10': 'z'},
+    const {'1': 'visibility', '3': 4, '4': 1, '5': 2, '10': 'visibility'},
+    const {'1': 'presence', '3': 5, '4': 1, '5': 2, '10': 'presence'},
+  ],
+};
+
+const NormalizedLandmarkList$json = const {
+  '1': 'NormalizedLandmarkList',
+  '2': const [
+    const {'1': 'landmark', '3': 1, '4': 3, '5': 11, '6': '.mediapipe.NormalizedLandmark', '10': 'landmark'},
+  ],
+};
+
